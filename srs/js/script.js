5b3b042dfddc8a92963d0a867b8d979cb68eda4a
Yroslav <yr30082005@gmail.com>
2020-04-10 19:24:27 +0300

    mode js file

--- srs/js/script.js
+++ srs/js/script.js
@@ -1,49 +1,23 @@
 'use strict';
 
-let a = 5;
-console.log(a)
-var number = 5;
-var string = "Hello";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false,
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-console.log(persone["name"]);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-let arr = ['plum.png','orange.jpg','apple.bmp'];
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-console.log(arr[0]);
-
-//alert("Hello World!");
-
-//let anwser = confirm("Are you here");
-
-//console.log(anwser);
-
-//let anwser = +prompt("Есть ли вам 18", "Да");
-
-//console.log(typeof(anwser));
-
-//console.log("arr" + "-object");
-//console.log(4 + +"- object");
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-console.log("2" === 2);
-
-let isChecked = false,
-    isClose = false;
-
-console.log(isChecked || !isClose);
+alert(appData.budget / 30);
